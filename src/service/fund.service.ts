106efd2ab2958b0dd199b0610f8b8adb64bd5494
tingkoo <guting1009@gmail.com>
2021-09-01 23:37:06 -0400

    fix bug

--- src/service/fund.service.ts
+++ src/service/fund.service.ts
@@ -1,11 +1,10 @@
 import promistPool from "../db/pool";
 import { Request, Response } from 'express';
-import log from "../logger";
 
 export async function findAll(req: Request, res: Response) {
     const sql: string = 'select * from student';
 
-    const [rows, fields] = await promistPool.query(sql);
+    const [rows, fields] = await promistPool.execute(sql);
 
     return rows;
 
